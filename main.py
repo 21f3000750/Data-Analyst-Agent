722f2e076dda5240296f9d8dc566715a9994de11
21f3000750 <21f3000750@ds.study.iitm.ac.in>
2025-08-10 15:37:05 +0530

    Deployment Changes

--- main.py
+++ main.py
@@ -70,6 +70,6 @@ def read_root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
     # To run the server, use the command: uvicorn main:app --reload
 # To run the server, use the command: uvicorn main:app --reload
